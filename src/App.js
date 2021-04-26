d892c7a76c78d4d42ee29b1483eb59357710e872
martin lowe <martinlowe673@gmail.com>
2021-04-26 07:29:17 +0100

    added api endpoint

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import PokemonList from "./PokemonList";
 
 function App() {
   const [pokemonData, setPokemondata] = useState([]);
-  const [nextURL, setNextURL];
-  return <PokemonList pokemon={pokemon} />;
+  const [nextURL, setNextURL] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    async fetchData() {
+      let response = await getAllPokemon(initialURL);
+      setNextURL(response.next);
+      setPrevUrl(response.previous);
+      setLoading(false);
+    }
+    }
+  }, [])
+  return (
+    <div>
+      { loading ? <h1>Loading...</h1> : (
+        <h1>Data is fetched</h1>
+      )}
+    </div>
+
+  );
 }
 
 export default App;
---
+++ src/components/services/pokemon.js
@@ -0,0 +1,11 @@
+
+
+export async function getAllPokemon(url) {
+  return new Promise(resolve, reject) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+      resolve(data);
+    })
+  };
+}
